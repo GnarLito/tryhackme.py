03a4e0a6fa34868bdf7aadedec4e9dc75bd08320
gnarlito <gnarlito35@gmail.com>
2021-12-09 23:57:14 +0100

    http options pass through

--- tryhackme/http.py
+++ tryhackme/http.py
@@ -14,11 +14,12 @@ POST='post'
 
 class HTTPClient:
     __CSRF_token_regex = re.compile("const csrfToken[ ]{0,1}=[ ]{0,1}[\"|'](.{36})[\"|']")
-    __Username_regex = re.compile("const username[ ]{0,1}=[ ]{0,1}[\"|'](.{1,16})[\"|']")
+    __Username_regex   = re.compile("const username[ ]{0,1}=[ ]{0,1}[\"|'](.{1,16})[\"|']")
+    
     def __init__(self, session=None):
         self.authenticated = False
         self.__session = requests.Session()
-        self.satic_session = requests.Session()
+        self.static_session = requests.Session()
         self.connect_sid = None
         self._CSRF_token = None
         self.username = None
@@ -71,8 +72,7 @@ class HTTPClient:
         settings = kwargs.pop('settings', {})
         
         if settings.get("static", False):
-            session = self.satic_session
-        
+            session = self.static_session
         
         headers = {
             'User-Agent': self.user_agent
@@ -255,168 +255,168 @@ class RouteList:
 class HTTP(request_cog, HTTPClient):
     # * normal site calls
     
-    def get_server_time(self):
-        return self.request(RouteList.get_server_time())
-    def get_site_stats(self):
-        return self.request(RouteList.get_site_stats())
-    def get_practise_rooms(self): 
-        return self.request(RouteList.get_practise_rooms())
-    def get_serie(self, show, serie_code):
-        return self.request(RouteList.get_series(show=show, options={"name": serie_code}))
-    def get_series(self, show):
-        return self.request(RouteList.get_series(show=show))
-    def get_glossary_terms(self): 
-        return self.request(RouteList.get_glossary_terms())
+    def get_server_time(self, **request_options):
+        return self.request(RouteList.get_server_time()**request_options)
+    def get_site_stats(self, **request_options):
+        return self.request(RouteList.get_site_stats()**request_options)
+    def get_practise_rooms(self, **request_options): 
+        return self.request(RouteList.get_practise_rooms()**request_options)
+    def get_serie(self, show, serie_code, **request_options):
+        return self.request(RouteList.get_series(show=show, options={"name": serie_code}), **request_options)
+    def get_series(self, show, **request_options):
+        return self.request(RouteList.get_series(show=show), **request_options)
+    def get_glossary_terms(self, **request_options): 
+        return self.request(RouteList.get_glossary_terms()**request_options)
     
     # * Leaderboards
     
-    def get_leaderboards(self, country: _county_types=None, type:_leaderboard_types=None):
-        return self.request(RouteList.get_leaderboards(country=country.to_lower_case(), type=type))
-    def get_koth_leaderboards(self, country: _county_types=None, type:_leaderboard_types=None):
-        return self.request(RouteList.get_koth_leaderboards(country=country.to_lower_case(), type=type))
+    def get_leaderboards(self, country: _county_types=None, type:_leaderboard_types=None, **request_options):
+        return self.request(RouteList.get_leaderboards(country=country.to_lower_case(), type=type), **request_options)
+    def get_koth_leaderboards(self, country: _county_types=None, type:_leaderboard_types=None, **request_options):
+        return self.request(RouteList.get_koth_leaderboards(country=country.to_lower_case(), type=type), **request_options)
 
     # * networks
     
-    def get_network(self, network_code): 
-        return self.request(RouteList.get_network(network_code=network_code))
-    def get_networks(self): 
-        return self.request(RouteList.get_networks())
-    def get_network_cost(self, network_code): 
-        return self.request(RouteList.get_networks(network_code=network_code))
+    def get_network(self, network_code, **request_options): 
+        return self.request(RouteList.get_network(network_code=network_code), **request_options)
+    def get_networks(self, **request_options): 
+        return self.request(RouteList.get_networks(),**request_options)
+    def get_network_cost(self, network_code, **request_options): 
+        return self.request(RouteList.get_networks(network_code=network_code), **request_options)
     
     # * account
     
-    def get_subscription_cost(self): 
-        return self.request(RouteList.get_subscription_cost())
+    def get_subscription_cost(self, **request_options): 
+        return self.request(RouteList.get_subscription_cost(), **request_options)
     
     # * paths
     
-    def get_path(self, path_code):
-        return self.request(RouteList.get_path(path_code=path_code))
-    def get_public_paths(self):
-        return self.request(RouteList.get_public_paths())
-    def get_path_summary(self):
-        return self.request(RouteList.get_path_summary())
+    def get_path(self, path_code, **request_options):
+        return self.request(RouteList.get_path(path_code=path_code), **request_options)
+    def get_public_paths(self, **request_options):
+        return self.request(RouteList.get_public_paths(), **request_options)
+    def get_path_summary(self, **request_options):
+        return self.request(RouteList.get_path_summary(), **request_options)
     
     # * modules
     
-    def get_modules_summary(self):
-        return self.request(RouteList.get_modules_summary())
-    def get_module(self, module_code):
-        return self.request(RouteList.get_module(module_code))
+    def get_modules_summary(self, **request_options):
+        return self.request(RouteList.get_modules_summary(), **request_options)
+    def get_module(self, module_code, **request_options):
+        return self.request(RouteList.get_module(module_code), **request_options)
     
     # * games
     
-    def get_machine_pool(self):
-        return self.request(RouteList.get_machine_pool())
-    def get_game_detail(self, game_code):
-        return self.request(RouteList.get_game_detail(game_code=game_code))
-    def get_recent_games(self):
-        return self.request(RouteList.get_recent_games())
-    def get_user_games(self):
-        return self.request(RouteList.get_user_games())
-    def get_game_tickets_won(self, username):
-        return self.request(RouteList.get_game_tickets_won(username=username))
-    def post_join_koth(self):
-        return self.request(RouteList.post_join_koth())
-    def post_new_koth(self):
-        return self.request(RouteList.post_new_koth())
+    def get_machine_pool(self, **request_options):
+        return self.request(RouteList.get_machine_pool(), **request_options)
+    def get_game_detail(self, game_code, **request_options):
+        return self.request(RouteList.get_game_detail(game_code=game_code), **request_options)
+    def get_recent_games(self, **request_options):
+        return self.request(RouteList.get_recent_games(), **request_options)
+    def get_user_games(self, **request_options):
+        return self.request(RouteList.get_user_games(), **request_options)
+    def get_game_tickets_won(self, username, **request_options):
+        return self.request(RouteList.get_game_tickets_won(username=username), **request_options)
+    def post_join_koth(self, **request_options):
+        return self.request(RouteList.post_join_koth(), **request_options)
+    def post_new_koth(self, **request_options):
+        return self.request(RouteList.post_new_koth(), **request_options)
     
     # * VPN
     
-    def get_available_vpns(self, type : _vpn_types):
-        return self.request(RouteList.get_available_vpns(options={"type": type}))
-    def get_vpn_info(self):
-        return self.request(RouteList.get_vpn_info())
+    def get_available_vpns(self, type : _vpn_types, **request_options):
+        return self.request(RouteList.get_available_vpns(options={"type": type}), **request_options)
+    def get_vpn_info(self, **request_options):
+        return self.request(RouteList.get_vpn_info(), **request_options)
     
     # * VM
 
-    def get_machine_running(self):
-        return self.request(RouteList.get_machine_running())
-    def post_renew_machine(self, room_code):
-        return self.request(RouteList.post_renew_machine(), json={"code": room_code, "_csrf": ""})
-    def post_terminate_machine(self, room_code):
-        return self.request(RouteList.post_terminate_machine(), json={"code": room_code, "_csrf": ""})
+    def get_machine_running(self, **request_options):
+        return self.request(RouteList.get_machine_running(), **request_options)
+    def post_renew_machine(self, room_code, **request_options):
+        return self.request(RouteList.post_renew_machine(), json={"code": room_code, "_csrf": ""}, **request_options)
+    def post_terminate_machine(self, room_code, **request_options):
+        return self.request(RouteList.post_terminate_machine(), json={"code": room_code, "_csrf": ""}, **request_options)
     
     # * user -badge
     
-    def get_own_badges(self):
-        return self.request(RouteList.get_own_badges())
-    def get_user_badges(self, username):
-        return self.request(RouteList.get_user_badges(username=username))
-    def get_all_badges(self):
-        return self.request(RouteList.get_all_badges())
+    def get_own_badges(self, **request_options):
+        return self.request(RouteList.get_own_badges(), **request_options)
+    def get_user_badges(self, username, **request_options):
+        return self.request(RouteList.get_user_badges(username=username), **request_options)
+    def get_all_badges(self, **request_options):
+        return self.request(RouteList.get_all_badges(), **request_options)
     
     # * user -team
     
-    def get_team_info(self): 
-        return self.request(RouteList.get_team_info())
+    def get_team_info(self, **request_options): 
+        return self.request(RouteList.get_team_info(), **request_options)
     
     # * user -notifications
     
-    def get_unseen_notifications(self):
-        return self.request(RouteList.get_unseen_notifications())
-    def get_all_notifications(self):
-        return self.request(RouteList.get_all_notifications())
+    def get_unseen_notifications(self, **request_options):
+        return self.request(RouteList.get_unseen_notifications(), **request_options)
+    def get_all_notifications(self, **request_options):
+        return self.request(RouteList.get_all_notifications(), **request_options)
 
     # * user -messages
 
-    def get_unseen_messages(self):
-        return self.request(RouteList.get_unseen_messages())
-    def get_all_group_messages(self):
-        return self.request(RouteList.get_all_group_messages())
-    def get_group_messages(self, group_id):
-        return self.request(RouteList.get_group_messages(group_id))
+    def get_unseen_messages(self, **request_options):
+        return self.request(RouteList.get_unseen_messages(), **request_options)
+    def get_all_group_messages(self, **request_options):
+        return self.request(RouteList.get_all_group_messages(), **request_options)
+    def get_group_messages(self, group_id, **request_options):
+        return self.request(RouteList.get_group_messages(group_id), **request_options)
     
     # * user -room
 
-    def get_user_completed_rooms_count(self, username):
-        return self.request(RouteList.get_user_completed_rooms_count(username=username))
-    def get_user_completed_rooms(self, username, limit:int=10, page:int=1):
-        return self.request(RouteList.get_user_completed_rooms(username=username, options={"limit": limit, "page": page}))
-    def get_user_created_rooms(self, username, limit:int=10, page:int=1):
-        return self.request(RouteList.get_user_created_rooms(username=username, options={"limit": limit, "page": page}))
+    def get_user_completed_rooms_count(self, username, **request_options):
+        return self.request(RouteList.get_user_completed_rooms_count(username=username), **request_options)
+    def get_user_completed_rooms(self, username, limit:int=10, page:int=1, **request_options):
+        return self.request(RouteList.get_user_completed_rooms(username=username, options={"limit": limit, "page": page}), **request_options)
+    def get_user_created_rooms(self, username, limit:int=10, page:int=1, **request_options):
+        return self.request(RouteList.get_user_created_rooms(username=username, options={"limit": limit, "page": page}), **request_options)
 
     # * user
 
-    def get_user_rank(self, username : _notNone_check):
-        return self.request(RouteList.get_user_rank(username=username))
-    def get_user_activty(self, username : _notNone_check):
-        return self.request(RouteList.get_user_activty(username=username))
-    def get_all_friends(self):
-        return self.request(RouteList.get_all_friends())
-    def get_discord_user(self, username : _notNone_check):
-        return self.request(RouteList.get_discord_user(username=username))
-    def get_user_exist(self, username : _notNone_check):
-        return self.request(RouteList.get_user_exist(username=username))
-    def search_user(self, username : _notNone_check):
-        return self.request(RouteList.search_user(username=username))
+    def get_user_rank(self, username : _notNone_check, **request_options):
+        return self.request(RouteList.get_user_rank(username=username), **request_options)
+    def get_user_activty(self, username : _notNone_check, **request_options):
+        return self.request(RouteList.get_user_activty(username=username), **request_options)
+    def get_all_friends(self, **request_options):
+        return self.request(RouteList.get_all_friends(), **request_options)
+    def get_discord_user(self, username : _notNone_check, **request_options):
+        return self.request(RouteList.get_discord_user(username=username), **request_options)
+    def get_user_exist(self, username : _notNone_check, **request_options):
+        return self.request(RouteList.get_user_exist(username=username), **request_options)
+    def search_user(self, username : _notNone_check, **request_options):
+        return self.request(RouteList.search_user(username=username), **request_options)
 
     # * room
 
-    def get_new_rooms(self):
-        return self.request(RouteList.get_new_rooms())
-    def get_recommended_rooms(self):
-        return self.request(RouteList.get_recommended_rooms())
-    def get_questions_answered(self):
-        return self.request(RouteList.get_questions_answered())
-    def get_joined_rooms(self):
-        return self.request(RouteList.get_joined_rooms())
-    def get_room_percentages(self, room_codes):
-        return self.request(RouteList.get_room_percetages(), json={"rooms": room_codes})
-    def get_room_scoreboard(self, room_code):
-        return self.request(RouteList.get_room_scoreboard(room_code=room_code))
-    def get_room_votes(self, room_code):
-        return self.request(RouteList.get_room_votes(room_code=room_code))
-    def get_room_details(self, room_code, loadWriteUps: bool=True, loadCreators: bool=True, loadUser: bool=True):
-        return self.request(RouteList.get_room_details(room_code=room_code, options={"loadWriteUps": loadWriteUps, "loadCreators": loadCreators, "loadUser": loadUser})).get(room_code)
+    def get_new_rooms(self, **request_options):
+        return self.request(RouteList.get_new_rooms(), **request_options)
+    def get_recommended_rooms(self, **request_options):
+        return self.request(RouteList.get_recommended_rooms(), **request_options)
+    def get_questions_answered(self, **request_options):
+        return self.request(RouteList.get_questions_answered(), **request_options)
+    def get_joined_rooms(self, **request_options):
+        return self.request(RouteList.get_joined_rooms(), **request_options)
+    def get_room_percentages(self, room_codes, **request_options):
+        return self.request(RouteList.get_room_percetages(), json={"rooms": room_codes}, **request_options)
+    def get_room_scoreboard(self, room_code, **request_options):
+        return self.request(RouteList.get_room_scoreboard(room_code=room_code), **request_options)
+    def get_room_votes(self, room_code, **request_options):
+        return self.request(RouteList.get_room_votes(room_code=room_code), **request_options)
+    def get_room_details(self, room_code, loadWriteUps: bool=True, loadCreators: bool=True, loadUser: bool=True, **request_options):
+        return self.request(RouteList.get_room_details(room_code=room_code, options={"loadWriteUps": loadWriteUps, "loadCreators": loadCreators, "loadUser": loadUser}), **request_options).get(room_code, {})
     def get_room_tasks(self, room_code, **request_options):
         return self.request(RouteList.get_room_tasks(room_code=room_code), **request_options)
-    def post_room_answer(self, room_code, taskNo: int, questionNo: int, answer: str):
-        return self.request(RouteList.post_room_answer(room_code=room_code), json={"_csrf": "", "taskNo": taskNo, "questionNo": questionNo, "answer": answer})
-    def post_deploy_machine(self, room_code, uploadId): # ? csrf token
-        return self.request(RouteList.post_deploy_machine(), json={"roomCode": room_code, "id": uploadId, "_csrf": ""})
-    def post_reset_room_progress(self, room_code): # ? csrf token
-        return self.request(RouteList.post_reset_room_progress(), json={"code": room_code, "_csrf": ""})
-    def post_leave_room(self, room_code): # ? csrf token
-        return self.request(RouteList.post_leave_room(), json={"code": room_code, "_csrf": ""})
+    def post_room_answer(self, room_code, taskNo: int, questionNo: int, answer: str, **request_options):
+        return self.request(RouteList.post_room_answer(room_code=room_code), json={"_csrf": "", "taskNo": taskNo, "questionNo": questionNo, "answer": answer}, **request_options)
+    def post_deploy_machine(self, room_code, uploadId, **request_options): # ? csrf token
+        return self.request(RouteList.post_deploy_machine(), json={"roomCode": room_code, "id": uploadId, "_csrf": ""}, **request_options)
+    def post_reset_room_progress(self, room_code, **request_options): # ? csrf token
+        return self.request(RouteList.post_reset_room_progress(), json={"code": room_code, "_csrf": ""}, **request_options)
+    def post_leave_room(self, room_code, **request_options): # ? csrf token
+        return self.request(RouteList.post_leave_room(), json={"code": room_code, "_csrf": ""}, **request_options)
